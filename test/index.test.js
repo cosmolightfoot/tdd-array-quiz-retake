c1b2e08a2e82f7fa54e8d6fbe0d7d217a03a8f54
Cosmo Kent <cosmolightfoot@gmail.com>
2019-04-12 15:19:41 -0700

    tests pass

--- test/index.test.js
+++ test/index.test.js
@@ -3,9 +3,9 @@ const isBig = require('../lib/index');
 describe('quiz tests', () => {
   it('removes numbers less than 5, adds `Wow!`', () => {
     expect(isBig([1, 2, 3, 4, 5, 6, 7, 8])).toEqual([
-      'Wow 6 is Big!',
-      'Wow 7 is Big', 
-      'Wow 8 is Big'
+      'Wow, 6 is Big!',
+      'Wow, 7 is Big!', 
+      'Wow, 8 is Big!'
     ]);
   });
 });
